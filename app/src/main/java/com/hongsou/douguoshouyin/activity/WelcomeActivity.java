a091c34e829ee313d4ef3d38dece78289e5db2a8
lpc <736458919@qq.com>
2018-07-24 20:43:38 +0800

    添加6.0权限请求

--- app/src/main/java/com/hongsou/douguoshouyin/activity/WelcomeActivity.java
+++ app/src/main/java/com/hongsou/douguoshouyin/activity/WelcomeActivity.java
@@ -1,15 +1,21 @@
 package com.hongsou.douguoshouyin.activity;
 
 
+import android.Manifest;
 import android.content.Intent;
 import android.util.Log;
 
 import com.hongsou.douguoshouyin.R;
 import com.hongsou.douguoshouyin.activity.login.LoginActivity;
 import com.hongsou.douguoshouyin.base.BaseActivity;
+import com.hongsou.douguoshouyin.tool.ToastUtil;
+import com.tbruyelle.rxpermissions2.Permission;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import java.util.Timer;
 import java.util.TimerTask;
+
+import io.reactivex.functions.Consumer;
 
 /**
  * 幻影页面
@@ -33,6 +39,24 @@ public class WelcomeActivity extends BaseActivity {
     @Override
     public void initView() {
         initTimer();
+        RxPermissions rxPermission = new RxPermissions(this);
+        rxPermission
+                .requestEach(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.CALL_PHONE)
+                .subscribe(new Consumer<Permission>() {
+                    @Override
+                    public void accept(Permission permission) throws Exception {
+                        if (permission.granted) {
+
+                        } else {
+                            // 用户拒绝了该权限，并且选中『不再询问』
+                            ToastUtil.showToast("请打开相关权限，否则将影响使用");
+                        }
+                    }
+                });
     }
 
     /**
@@ -66,8 +90,6 @@ public class WelcomeActivity extends BaseActivity {
 
     @Override
     public void initData() {
-
-
     }
 
     @Override
