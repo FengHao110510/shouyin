af0473a2728532fef3148df3f7a12bf20092f27c
冯浩 <302917797@qq.com>
2018-08-10 10:26:11 +0800

    流水页面bug

--- app/src/main/java/com/hongsou/douguoshouyin/fragment/turnover/TurnoverTurnoverFragment.java
+++ app/src/main/java/com/hongsou/douguoshouyin/fragment/turnover/TurnoverTurnoverFragment.java
@@ -165,6 +165,9 @@ public class TurnoverTurnoverFragment extends BaseFragment {
             mTurnoverAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
                 @Override
                 public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                    if (turnoverMultipleItemList.get(position).getItemType()==TurnoverMultipleItem.TYPE_DATE){
+                        return;
+                    }
                     Intent itemIntent = new Intent(getActivity(), PaymentDetailActivity.class);
                     String batch = result.get(position).getBatch();
                     String paymentBatch = result.get(position).getPaymentBatch();
